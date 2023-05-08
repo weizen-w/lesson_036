2f79581f25a279204015afac9ca33d9bee953c1d
Wladimir <weizen.w@gmx.de>
2023-05-08 13:45:42 +0200

    feat: add method createOrderList

--- src/GasStation.java
+++ src/GasStation.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GasStation {
 
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("=== Заправочная станция ===");
+    System.out.println("Добро пожаловать! Как вас зовут?");
+    String name = scanner.nextLine();
+    ArrayList<Order> orderArrayList = createOrderList(name, scanner);
+    for (Order order : orderArrayList) {
+      order.print();
+    }
+  }
+
+  private static int readOrdersAmount(Scanner scanner) {
+    System.out.print("Введите количество заказов: ");
+    while (!scanner.hasNextInt()) {
+      String wrongLine = scanner.nextLine();
+      System.out.println("Неправильный формат целого числа: " + wrongLine);
+      System.out.print("Введите количество заказов: ");
+    }
+    int orders = scanner.nextInt();
+    scanner.nextLine();
+    return orders;
+  }
+
+  private static ArrayList<Order> createOrderList(String name, Scanner scanner) {
+    ArrayList<Order> orderList = new ArrayList<>();
+    int orders = readOrdersAmount(scanner);
+    for (int counter = 0; counter < orders; ++counter) {
+      orderList.add(Order.readOrder(name, scanner));
+    }
+    return orderList;
+  }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,60 @@
+import java.util.Scanner;
+
 public class Order {
 
+  private static final double PRICE = 2.0;
+
+  private enum PaymentMethod {
+    CARD,
+    CASH,
+  }
+
+  private final String name;
+  private final double quantity;
+  private final PaymentMethod paymentMethod;
+
+  public Order(String name, double quantity, PaymentMethod paymentMethod) {
+    this.name = name;
+    this.quantity = quantity;
+    this.paymentMethod = paymentMethod;
+  }
+
+  public static Order readOrder(String name, Scanner scanner) {
+    double quantity = readFuelQuantity(scanner);
+    PaymentMethod paymentMethod = readPaymentMethod(scanner);
+    return new Order(name, quantity, paymentMethod);
+  }
+
+  private static double readFuelQuantity(Scanner scanner) {
+    System.out.println("Сколько литров топлива вам нужно?");
+    while (!scanner.hasNextDouble()) {
+      String wrongLine = scanner.nextLine(); // пропустить строку
+      System.out.println("Неправильный формат дробного числа: " + wrongLine);
+      System.out.println("Введите количество топлива (в литрах):");
+    }
+    double quantity = scanner.nextDouble();
+    scanner.nextLine();
+    return quantity;
+  }
+
+  private static PaymentMethod readPaymentMethod(Scanner scanner) {
+    System.out.println("Как будете оплачивать, картой или наличными?");
+    String paymentMethod = scanner.nextLine();
+    if (paymentMethod.toLowerCase().contains("карт")) {
+      return PaymentMethod.CARD;
+    }
+    return PaymentMethod.CASH;
+  }
+
+  public void print() {
+    System.out.println("=== Ваш заказ ===");
+    System.out.println("Заказчик: " + name);
+    System.out.println("Количество топлива: " + quantity + " л");
+    double total = quantity * PRICE;
+    System.out.println("Стоимость заказа: " + total + " EUR");
+    switch (paymentMethod) {
+      case CARD -> System.out.println("Заказ должен быть оплачен картой");
+      case CASH -> System.out.println("Заказ должен быть оплачен наличными");
+    }
+  }
 }
